ed2efbca89319075e18c285c734698b49192fc73
Jared Krinke <jared.krinke@gmail.com>
2022-10-12 21:33:26 -0700

    Refactor test

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,21 +1,25 @@
 import { markdownToJSX } from "./main";
 import * as assert from "assert";
 
+function createExpected(jsx: string): string {
+    return `export default <>\n${jsx}\n</>;\n`;
+}
+
 describe("md2jsx", function () {
     it("One paragraph", function () {
-        assert.equal(markdownToJSX("Hi"), "export default <>\n<p>Hi</p>\n</>;\n");
+        assert.equal(markdownToJSX("Hi"), createExpected("<p>Hi</p>"));
     });
 
     it("Braces", function () {
-        assert.equal(markdownToJSX("{ \\{ \\} }"), "export default <>\n<p>{'{'} {'{'} {'}'} {'}'}</p>\n</>;\n");
+        assert.equal(markdownToJSX("{ \\{ \\} }"), createExpected("<p>{'{'} {'{'} {'}'} {'}'}</p>"));
     });
 
     it("Braces in inline code", function () {
-        assert.equal(markdownToJSX("`{ \\{ \\} }`"), "export default <>\n<p><code>{'{'} \\{'{'} \\{'}'} {'}'}</code></p>\n</>;\n");
+        assert.equal(markdownToJSX("`{ \\{ \\} }`"), createExpected("<p><code>{'{'} \\{'{'} \\{'}'} {'}'}</code></p>"));
     });
 
     it("Brackets in inline code", function () {
-        assert.equal(markdownToJSX("`<html>`"), "export default <>\n<p><code>&lt;html&gt;</code></p>\n</>;\n");
+        assert.equal(markdownToJSX("`<html>`"), createExpected("<p><code>&lt;html&gt;</code></p>"));
     });
 
     it("Fenced code with whitespace", function() {
@@ -23,6 +27,6 @@ describe("md2jsx", function () {
 for (let i = 0; i < array.length; i++) {
     console.log(\`<\${array[i]}>\`);
 }
-\`\`\``), "export default <>\n<pre><code>{`for (let i = 0; i < array.length; i++) {\n    console.log(\\`<\\${array[i]}>\\`);\n}`}</code></pre>\n</>;\n");
+\`\`\``), createExpected("<pre><code>{`for (let i = 0; i < array.length; i++) {\n    console.log(\\`<\\${array[i]}>\\`);\n}`}</code></pre>"));
     });
 });
